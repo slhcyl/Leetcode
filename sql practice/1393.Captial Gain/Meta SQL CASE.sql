6023e52f96c1e81602a588424e4d39af88dfb52d
slhcyl <slhcyl2023@gmail.com>
2024-04-04 11:26:48 -0500

    some minor change on comments

---
+++ sql practice/1393.Captial Gain/Meta SQL CASE.sql
@@ -0,0 +1,74 @@
+ /*
+ BACKGROUND:
+ 
+ The following schema is a subset of a relational database of a grocery store
+ chain. This chain sells many products of different product classes to its
+ customers across its different stores. It also conducts many different
+ promotion campaigns.
+ 
+ The relationship between the four tables we want to analyze is depicted below:
+ 
+        # sales                                # products
+        +------------------+---------+         +---------------------+---------+
+        | product_id       | INTEGER |>--------| product_id          | INTEGER |
+        | store_id         | INTEGER |    +---<| product_class_id    | INTEGER |
+        | customer_id      | INTEGER |    |    | brand_name          | VARCHAR |
+   +---<| promotion_id     | INTEGER |    |    | product_name        | VARCHAR |
+   |    | store_sales      | DECIMAL |    |    | is_low_fat_flg      | TINYINT |
+   |    | store_cost       | DECIMAL |    |    | is_recyclable_flg   | TINYINT |
+   |    | units_sold       | DECIMAL |    |    | gross_weight        | DECIMAL |
+   |    | transaction_date | DATE    |    |    | net_weight          | DECIMAL |
+   |    +------------------+---------+    |    +---------------------+---------+
+   |                                      |
+   |    # promotions                      |    # product_classes
+   |    +------------------+---------+    |    +---------------------+---------+
+   +----| promotion_id     | INTEGER |    +----| product_class_id    | INTEGER |
+        | promotion_name   | VARCHAR |         | product_subcategory | VARCHAR |
+        | media_type       | VARCHAR |         | product_category    | VARCHAR |
+        | cost             | DECIMAL |         | product_department  | VARCHAR |
+        | start_date       | DATE    |         | product_family      | VARCHAR |
+        | end_date         | DATE    |         +---------------------+---------+
+        +------------------+---------+
+ */
+ /*
+ PROMPT:
+ -- Of sales that had a valid promotion, the VP of marketing
+ -- wants to know what % of transactions occur on either
+ -- the very first day or the very last day of a promotion campaign.
+ 
+ 
+ EXPECTED OUTPUT:
+ Note: Please use the column name(s) specified in the expected output in your solution.
+ +-------------------------------------------------------------+
+ | pct_of_transactions_on_first_or_last_day_of_valid_promotion |
+ +-------------------------------------------------------------+
+ |                                         41.9047619047619048 |
+ +-------------------------------------------------------------+
+  Case
+Please refer to the schema and prompt on the right.
+Note that in this editor, you are using a SQLite database. In the interview you will run the queries against a PostgreSQL database. Yet knowing standard ANSI SQL is sufficient for the interview.
+Please feel free to collapse this panel to simulate a real interview experience.
+Below are hints you can use to write the correct query.
+HINTS
+How do we find all sales between the start and end date?
+How do we find sales with valid promotions?
+You can join both the sales and promotions tables based on the promotion_id.
+ -------------- PLEASE WRITE YOUR SQL SOLUTION BELOW THIS LINE ----------------
+ */
+SELECT SUM(CASE WHEN s.transaction_date = p.start_date OR s.transaction_date = p.end_date THEN 1 END)*1.0 /COUNT(*) * 100 AS pct_of_transactions_on_first_or_last_day_of_valid_promotion
+FROM sales s
+JOIN promotions p ON s.promotion_id = p.promotion_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
